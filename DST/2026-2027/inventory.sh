1cd8f590adbdd1ba6f17471f2a9d6e3aad27f492
Wenceslao González-Viñas <wens@unav.es>
2026-09-21 12:22:02 +0200

    Create inventory script for campaign data
    The script generates an inventory report for a specified campaign, counting the number of files in the raw data directory and categorizing them by type (TXT, CSV, LOG). It includes options for obtaining absolute paths and different methods for counting file types.

---
+++ DST/2026-2027/inventory.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+clear
+campanya=$1
+carpeta_dat="../$campanya/raw_data"
+carpeta_res="../results"
+mkdir -p $carpeta_res
+fichero_res="${carpeta_res}/inventario_${campanya}.txt"
+printf "Inventory report\n================\n\n" > $fichero_res
+
+# !! si se quisiera el directorio absoluto
+## OPCION A: (antes de la siguiente linea no comentada añadir, descomentando:)
+# carpeta_actual=$(pwd)
+# cd carpeta_dat
+# carpeta_dat=$(pwd)
+# cd carpeta_actual
+## OPCION B: (comandos nuevos, antes de la siguiente linea no comentada añadir, descomentando:))
+# carpeta_dat=$(realpath $carpeta_dat)
+printf "Directory: %s\n\n" $carpeta_dat >> $fichero_res
+
+printf "Number of files: %s\n" ${carpeta_dat}/* | wc -l >> $fichero_res
+printf "\n" >> $fichero_res
+
+## OPCION A
+printf "TXT files: %s\n" ${carpeta_dat}/*.[tT][xX][tT] | wc -l >> $fichero_res
+printf "CSV files: %s\n" ${carpeta_dat}/*.[cC][sS][vV] | wc -l >> $fichero_res
+printf "LOG files: %s\n" ${carpeta_dat}/*.[lL][oO][gG] | wc -l >> $fichero_res
+
+## OPCION B (comentar la opcion A y descomentar las siguientes 7 lineas:)
+# extensiones=$(ls -A $carpeta_dat | rev | cut -d. -f1 | rev | sort)
+# TXT_files=$(echo "$extensiones" | grep -i "txt"| wc -l) 
+# CSV_files=$(echo "$extensiones" | grep -i "csv"| wc -l) 
+# LOG_files=$(echo "$extensiones" | grep -i "log"| wc -l) 
+# printf "TXT files: %s\n" $TXT_files >> $fichero_res
+# printf "CSV files: %s\n" $CSV_files >> $fichero_res
+# printf "LOG files: %s\n" $LOG_files >> $fichero_res
+
+## OPCION C (comandos nuevos, comentar la opcion A y descomentar las siguientes 5 lineas:)
+# shopt -s nocaseglob
+# printf "TXT files: %s\n" ${carpeta_dat}/*.txt | wc -l >> $fichero_res
+# printf "CSV files: %s\n" ${carpeta_dat}/*.csv | wc -l >> $fichero_res
+# printf "LOG files: %s\n" ${carpeta_dat}/*.log | wc -l >> $fichero_res
+# shopt -u nocaseglob
+#
+
+printf "Inventario generado de la campaña seleccionada\n"
